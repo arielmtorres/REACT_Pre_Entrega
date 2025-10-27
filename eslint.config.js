36882d4f47e8a16a1cc3df90f77ac66dac82a8a5
arielmtorres <144166755+arielmtorres@users.noreply.github.com>
2025-10-27 04:48:09 -0300

    Refactor ESLint configuration for JavaScript files

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-<<<<<<< HEAD
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -28,29 +27,3 @@ export default defineConfig([
     },
   },
 ])
-=======
-import tseslint from 'typescript-eslint'
-
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
->>>>>>> 2488bbfd95e19d842a548d8fc5800ad9421d42a4
